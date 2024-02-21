1a51566829d655670dd256a3d8246b9b6298d32c
Sidra Iqbal <sidra1360@gmail.com>
2024-02-21 19:00:01 +0000

    feat: created facility component

---
+++ frontend/src/components/FacilityComponent/FacilityComponent.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Text, View } from 'react-native';
+
+export const FacilityComponent = () => {
+
+  const facilityList = [
+    "Wifi", "No Bright Lights", "Low Noise"
+  ]
+  
+
+
+  return (
+    <>
+      {/* Icon is a prop for the circle */}
+    </>
+  )
+}
