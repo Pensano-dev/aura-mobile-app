7d52047f3daaf202898ff54f861d6a15dcaff05b
Sidra Iqbal <sidra1360@gmail.com>
2024-02-22 16:13:33 +0000

    fix: updated component import

--- frontend/src/components/Facility/Facility.js
+++ frontend/src/components/Facility/Facility.js
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { styles } from "./FacilityStyles";
 
-export default FacilityComponent = ({ facilityName, iconName }) => {
+export default Facility = ({ facilityName, iconName }) => {
   return (
     <View style={styles.container}>
       <View style={styles.iconContainer}>
