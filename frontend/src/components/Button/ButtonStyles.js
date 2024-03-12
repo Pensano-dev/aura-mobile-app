fadca3407122043d64683f4b1c6b2abf9e630ccb
Chiara Pigoni <m.chiarapigoni@gmail.com>
2024-03-12 20:29:21 +0000

    glabal variable added, comment removed, react-navigation dependency added

--- frontend/src/components/Button/ButtonStyles.js
+++ frontend/src/components/Button/ButtonStyles.js
@@ -3,14 +3,14 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     elevation: 8,
-    backgroundColor: "#fff",
+    backgroundColor: "#2E8B57",
     borderRadius: 10,
     paddingVertical: 15,
     paddingHorizontal: 20
   },
   text: {
     fontSize: 18,
-    color: "#0e1111",
+    color: "#fff",
     fontWeight: "bold",
     alignSelf: "center",
     textTransform: "uppercase"
--- frontend/src/components/MenuBar/MenuBar.js
+++ frontend/src/components/MenuBar/MenuBar.js
@@ -1,9 +1,5 @@
 import React from "react";
 import { StyleSheet } from "react-native";
-//import { TouchableOpacity, Text } from "react-native";
-//import { createNativeStackNavigator } from "@react-navigation/native-stack";
-//import { NavigationContainer } from "@react-navigation/native";
-//import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { useNavigation } from "@react-navigation/native";
 import { View, Text, TouchableHighlight } from "react-native";
 import { styleVariables } from "../../styles/globalStyleVariables";
@@ -26,6 +22,6 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     padding: 20,
-    backgroundColor: "#A9A9A9" // I cannot set this color as globalStyleVariable.js background
+    backgroundColor: styleVariables.menuBarBackground
   }
 });
