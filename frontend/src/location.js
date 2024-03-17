98456a2d6b928a9bf54335b4b06a5ae98c6e5773
Sidra Iqbal <114309741+siqbal181@users.noreply.github.com>
2024-03-17 13:57:53 +0000

    chore: added location file for user location

---
+++ frontend/src/location.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { View, Alert } from 'react-native';
+import MyButton from './MyButton';
+import { getCurrentPositionAsync, useForegroundPermissions, PermissionStatus } from 'expo-location';
+
+const Location = () => {
+  const [locationPermissionInfo, requestPermission] = useForegroundPermissions();
+  
+  async function verifyPermissions() {
+    if (locationPermissionInfo.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+      return permissionResponse.granted;
+    }
+
+    if (locationPermissionInfo.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        'Insufficient permissions!',
+        'You need to grant location permissions to use this app.',
+        [{ 
+          text: 'Okay',
+          // onPress: async () => {
+          //   const permissionResponse = await requestPermission();
+          //   return permissionResponse.granted;
+          // }
+        }]
+      );
+      return false;
+    }
+
+    return true;
+  }
+
+  const getLocationHandler = async () => {
+    const hasPermission = await verifyPermissions();
+    if (!hasPermission) {
+      console.log('No permission')
+      return;
+    }
+
+    console.log('Getting location...');
+    const location = await getCurrentPositionAsync();
+    console.log(location);
+  }
+
+  return (
+    <View>
+      <MyButton icon="location" onPress={getLocationHandler}>Get Location</MyButton>
+    </View>
+  )
+}
+
+export default Location;
