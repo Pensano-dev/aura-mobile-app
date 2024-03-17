2006cb767e2dc916df71d2cf0db0b888092a0dc6
Sidra Iqbal <114309741+siqbal181@users.noreply.github.com>
2024-03-17 14:52:36 +0000

    feat: added location expo function to app

--- frontend/App.js
+++ frontend/App.js
@@ -2,10 +2,38 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import FacilitySelectorScreen from "./src/screens/FacilitySelectorScreen/FacilitySelectorScreen";
 import MenuBar from "./src/components/MenuBar/MenuBar";
+import { useState, useEffect } from 'react';
+
+import * as Location from 'expo-location';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    console.log('location', location)
+    console.log('stringified', JSON.stringify(location));
+  }
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
--- frontend/src/utils/location2.js
+++ frontend/src/utils/location2.js
@@ -1,23 +1,36 @@
-import { useState, useEffect } from 'react';
-import { Platform, Text, View, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View } from 'react-native';
 
 import * as Location from 'expo-location';
 
 export default function App() {
   const [location, setLocation] = useState(null);
-  const [errorMeg, setErrorMsg] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
 
   useEffect(() => {
     (async () => {
-
-
+      
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== 'granted') {
         setErrorMsg('Permission to access location was denied');
-        return
+        return;
       }
 
       let location = await Location.getCurrentPositionAsync({});
-    })
-  })
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
+  return (
+    <View>
+      <Text>{text}</Text>
+    </View>
+  );
 }
