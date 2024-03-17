aea3637b271a6650dff98dcf4f6532e36895b400
Pablo <pablito@me.com>
2024-03-17 15:46:05 +0000

    Added some dev notes, refactored getLocation func and set a default location.

--- frontend/App.js
+++ frontend/App.js
@@ -3,36 +3,48 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import FacilitySelectorScreen from "./src/screens/FacilitySelectorScreen/FacilitySelectorScreen";
 import MenuBar from "./src/components/MenuBar/MenuBar";
 import { useState, useEffect } from 'react';
+import { Alert } from 'react-native';
 
 import * as Location from 'expo-location';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
+  const [location, setLocation] = useState({
+    "coords": {
+      "accuracy": 0,
+      "altitude": null,
+      "altitudeAccuracy": -1,
+      "heading": null,
+      "latitude": 51.5076,
+      "longitude": -0.0994,
+      "speed": 0
+    },
+    "timestamp": 0
+  });
 
   useEffect(() => {
-    (async () => {
+    const getLocation = async () => {
       
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
+        Alert.alert(
+          'Location permissions denied',
+          'Aura uses the Tate Modern as the default location. You may grant Aura location permissions in your device settings.',
+          [{ 
+            text: 'Okay',
+          }]
+        );
+        console.log('location', location);
         return;
       }
 
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-    })();
-  }, []);
+      let userLocation = await Location.getCurrentPositionAsync({});
+      setLocation(userLocation);
+    };
 
-  let text = 'Waiting..';
-  if (errorMsg) {
-    text = errorMsg;
-  } else if (location) {
-    console.log('location', location)
-    console.log('stringified', JSON.stringify(location));
-  }
+    getLocation();
+  }, []);
 
   return (
     <NavigationContainer>
