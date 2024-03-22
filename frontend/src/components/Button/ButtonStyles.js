d370f509c11d572b21550977170030114e01553c
Luiz Santos <luizsantos@Luizs-MacBook-Air-2.local>
2024-03-22 12:19:03 -0300

    fix: reverted button style to how it was

--- frontend/src/components/Button/ButtonStyles.js
+++ frontend/src/components/Button/ButtonStyles.js
@@ -1,20 +1,18 @@
 import { StyleSheet } from "react-native";
 
-const primaryBackgroundColor = "#648FFF";
-
-export const styles = (isPrimary) => StyleSheet.create({
+export const styles = StyleSheet.create({
   container: {
     elevation: 8,
-    backgroundColor:  primaryBackgroundColor,
+    backgroundColor: "#2E8B57",
     borderRadius: 10,
     paddingVertical: 15,
     paddingHorizontal: 20
   },
   text: {
     fontSize: 18,
-    color:  '#fff',
+    color: "#fff",
     fontWeight: "bold",
     alignSelf: "center",
     textTransform: "uppercase"
-  },
+  }
 });
